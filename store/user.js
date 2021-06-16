f31cc96e4afc9cefb3b2f74c9c512715f19afb13
Saša Gošović <39879027+SaleCar@users.noreply.github.com>
2021-06-16 22:59:38 +0200

    Add files via upload

--- store/user.js
+++ store/user.js
@@ -1,1 +1,47 @@
+export const user = Vue.reactive({
 
+    name: null,
+    email: null,
+    token: null,
+
+
+    //login dialog
+    loginDialog: {
+        show: null,
+        loading: null,
+        email: null,
+        password: null,
+        pwdVisible: null,
+        reset: () => {
+            user.loginDialog.show = user.loginDialog.loading = user.loginDialog.email = user.loginDialog.password = user.loginDialog.pwdVisible = null
+        }
+    },
+
+
+    //login user to server
+    login: () => {
+        if (user.loginDialog.email != 'admin@test.com' || user.loginDialog.password != 'admin') return null
+        const res = {
+            name: 'Jane Smith',
+            email: 'admin@test.com',
+            token: 'a9bedabb5f8f6432'
+        }
+        Object.assign(user, res)
+        localStorage.setItem('user', JSON.stringify(res))
+        user.loginDialog.reset()
+        return 'ok'
+    },
+
+
+    logout: () => {
+        user.reset()
+        localStorage.removeItem('user')
+        if (Vue.$router.currentRoute.value.path != '/') Vue.$router.push('/')
+    },
+
+
+    reset: () => {
+        user.name = user.email = user.token = null
+    }
+        
+})

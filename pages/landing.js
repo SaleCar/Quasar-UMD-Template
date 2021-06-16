e6064c9595cc1bb8a5d8f2ac75d490ce97db3e42
Saša Gošović <39879027+SaleCar@users.noreply.github.com>
2021-06-16 22:58:58 +0200

    Add files via upload

---
+++ pages/landing.js
@@ -0,0 +1,35 @@
+export default {
+    name: 'Landing page',
+
+    template: ` 
+        <!-- home -->            
+        <section id="home" class="row items-center justify-center" style="height: 600px;">  
+            <p class="text-h2 text-weight-bolder">home page</p>             
+        </section>
+        
+        <!-- features -->
+        <section id="features" class="row items-center justify-center" style="height: 600px;">
+            <p class="text-h2 text-weight-bolder">features</p>
+        </section>
+
+        <!-- plans -->
+        <section id="plans" class="row items-center justify-center" style="height: 600px;">
+            <p class="text-h2 text-weight-bolder">pricing plans</p>
+        </section>
+
+        <!-- about -->
+        <section id="about" class="row items-center justify-center" style="height: 600px;">
+            <p class="text-h2 text-weight-bolder">about us</p>
+        </section>
+
+        <!-- contact form -->
+        <section id="contact" class="row items-center justify-center" style="height: 600px;">
+            <p class="text-h2 text-weight-bolder">contact form</p>
+        </section>
+
+        <!-- footer -->
+        <section class="row items-center justify-center" style="height: 50px;">
+            <span class="text-caption">Copyright © 2021 Quasar Template. All rights reserved.</span>
+        </section>        
+    `,
+};
